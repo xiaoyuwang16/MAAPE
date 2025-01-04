607992e22b313adef13e7248dd16336e0763d05d
xiaoyuwang16 <xiaoyuwang.bnu@gmail.com>
2025-01-04 11:51:06 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    DATA_DIR = "data"
-    EMBEDDINGS_FILE = f"{DATA_DIR}/P450_embeddings_normalized_pca.npy"
-    PATHS_FILE = f"{DATA_DIR}/P450_paths_with_similar_indices_pca.pkl"
-    EDGES_FILE = f"{DATA_DIR}/P450_all_edges_pca.pkl"
-    EDGES_DATA_FILE = f"{DATA_DIR}/P450_all_edges_data_pca.pkl"
-    KNN_EDGES_FILE = f"{DATA_DIR}/P450_knn_graph_edges.txt"
-    NEW_WEIGHTS_FILE = f"{DATA_DIR}/P450_new_edge_weights_pca.pkl"
